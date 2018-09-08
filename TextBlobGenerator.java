dd7f145e73908d554b8c6cc811e32af149f7761f
babatunde adewuyi <babatundeadewuyi@babatundes-MBP.default>
2018-09-08 19:54:00 +0100

    More refactoring

--- TextBlobGenerator.java
+++ TextBlobGenerator.java
@@ -35,70 +35,84 @@
  */
 public class TextBlobGenerator {
 
-
 	private String[] letterss = {"a","b", "c", "d", "e", "f"};
-
-
 
 	private final int addLettersFor;
 	private int padLeftFor;
 	private int padRightFor;
-
-
 	String paddingStyle;
-
 	private String blob;
+	static String instructions;
 
 	public TextBlobGenerator() {
 
-		String instructions = "6,8,6,paddingStyle:left";
-//		String instructions = "addLettersFor:9-loops,padLeftFor:99-loops,padRightFor:999-loops,paddingStyle:left";
-
-		addLettersFor = Integer.parseInt(instructions.split(",")[0]);
-		padLeftFor = Integer.parseInt(instructions.split(",")[1]);
-		padRightFor = Integer.parseInt(instructions.split(",")[2]);
-		paddingStyle = (instructions.split(",")[3]).split(":")[1];
+		addLettersFor = getNumberOfLetters(instructions);
+		padLeftFor = getNumberOfLeftPadding(instructions);
+		padRightFor = getNumberOfRightPadding(instructions);
+		paddingStyle = getPaddingStyle(instructions);
 		blob = "";
 
-		
-		if(paddingStyle.trim() == "left")
+		for(int i=0; i<addLettersFor; i++){
+			blob = blob + letterss[i];
+		}
+
+		if(paddingStyle.equals("left"))
 		{
-
-			for(int i=0; i<addLettersFor; i++){
-				blob = letterss[i] + blob;
-			}
-
 			for(int i=0; i<padLeftFor; i++){
-			   blob = "=" + blob;
+				blob = "=" + blob;
 			}
-				
-
-		} 
-	
-
-		else{
-
-			for(int j=0; j<addLettersFor; j++){
-			
-				blob=letterss[j] + blob;
-			}
-
+		}
+		else if (paddingStyle.equals("right")){
 			for(int j=0; j<padRightFor; j++){
 				blob = blob + "=";
 			}
-				
 		}
-
 	}
 
 	public String getBlob() {
 		return blob;
 	}
 
+	public String getPaddingStyle(String inst){
+		if(numberOfInstructions(inst) < 4){
+			return "none";
+		}
+		else{
+			String firstSplit = inst.split(",")[3];
+			String secSplit= firstSplit.split(":")[1];
+			return secSplit;
+		}
+	}
+
+	public int numberOfInstructions(String inst){
+		int count = inst.split(",").length;
+		return count;
+	}
+
+	public int getNumberOfLetters(String inst){
+		String str = tidyUp(inst).split(",")[0];
+		return Integer.parseInt(str.split(":")[1]);
+	}
+
+	public int getNumberOfLeftPadding(String inst){
+		String str = tidyUp(inst).split(",")[1];
+		return Integer.parseInt(str.split(":")[1]);
+	}
+
+	public int getNumberOfRightPadding(String inst){
+		String str = tidyUp(inst).split(",")[2];
+		return Integer.parseInt(str.split(":")[1]);
+	}
+
+	public static String tidyUp(String inst){
+		return inst.replace("-loops", "");
+	}
 
 	public static void main(String[] args) {
+		instructions = "addLettersFor:6-loops,padLeftFor:4-loops,padRightFor:4-loops";
+//		instructions = "addLettersFor:4-loops,padLeftFor:10-loops,padRightFor:5-loops,paddingStyle:left";
+//		instructions = "addLettersFor:3-loops,padLeftFor:10-loops,padRightFor:5-loops,paddingStyle:right";
 		System.out.println(new TextBlobGenerator().getBlob());
-
 	}
 
 
